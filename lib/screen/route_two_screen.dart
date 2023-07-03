4815ab741f7f928c0857e1513667130d6c0094ae
AWKRID <minzoo202738@gmail.com>
2023-07-03 15:16:31 +0900

    push 기능 이해

--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nagivation/screen/route_three_screen.dart';
 
 import '../layout/main_layout.dart';
 
@@ -21,9 +22,40 @@ class RouteTwoScreen extends StatelessWidget {
           },
           child: Text('Pop'),
         ),
-        ElevatedButton(onPressed: (){
-          Navigator.of(context).pushNamed('/three', arguments: 999);
-        }, child: Text('Push Named'))
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed('/three', arguments: 999);
+          },
+          child: Text('Push Named'),
+        ),
+        // pushReplacement - 기존 screen을 대체하고 자신이 stack에 들어간다.
+        // ElevatedButton(
+        //   onPressed: () {
+        //     Navigator.of(context).pushReplacement(
+        //       MaterialPageRoute(
+        //         builder: (_) => RouteThreeScreen(),
+        //       ),
+        //     );
+        //   },
+        //   child: Text(''),
+        // ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('/three');
+          },
+          child: Text(''),
+        ),
+        // pushAndRemoveUntil
+        // false: navigation stack의 원소를 삭제
+        // true: 제거 X
+
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (_) => RouteThreeScreen()),
+                  (route) => route.settings.name == '/');
+            },
+            child: Text('push/removeuntil'))
       ],
     );
   }
